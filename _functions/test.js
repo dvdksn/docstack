27dd82c71bdcf77c2332f85a8540b110c327996c
David Karlsson <davidkarlsson0@gmail.com>
2021-02-22 22:30:53 +0100

    Testing database fetch with serverless function

--- _functions/test.js
+++ _functions/test.js
@@ -1,6 +1,37 @@
-exports.handler = async (event, context) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Success!"})
+import { MongoClient } from "mongodb"
+
+async function getData() {
+    const uri = process.env.MONGO_URI
+    const client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+
+    try {
+        await client.connect()
+        const test = await client
+            .db("dev")
+            .collection("stacks")
+            .findOne({ name: "MkDocs"})
+    } catch (err) {
+        console.log(err)
+    } finally {
+        await client.close();
+    }
+}
+
+export const handler = async (event, context) => {
+    try {
+        const data = await getData()
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ id: data._id })
+        }
+    } catch (err) {
+        console.log(err)
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: err.message })
+        }
     }
 }
